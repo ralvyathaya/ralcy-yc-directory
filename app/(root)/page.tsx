a54a95591e27d9b958d61c2789c6af46f1276155
rxlvx <108793709+ralvyathaya@users.noreply.github.com>
2025-02-19 17:37:24 +0700

    Implement startup search functionality with SearchForm and SearchFormReset components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
-// app/page.tsx
-export default function Home() {
+import SearchForm from "../components/SearchForm";
+
+// Home component that handles search parameters
+export default async function Home({ searchParams }: {
+  searchParams: Promise<{ query?: string }>; // Corrected type for searchParams
+}) { 
+  const query  = (await searchParams).query; // Destructured query for clarity
   return (
     <>
-      <h1 className="heading">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+        </p>
+        <SearchForm query={query} /> {/* Removed unnecessary spaces */}
+      </section>
     </>
   );
 }
